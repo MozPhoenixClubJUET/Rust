9b0c609c60dd5c26311122db93b4fede082a95ad
Emilien Fugier <mail@emilienfugier.xyz>
2018-10-02 17:16:56 +0200

    Removed the main function
    Didn

--- Algorithms/factorial.rs
+++ Algorithms/factorial.rs
@@ -1,9 +1,4 @@
-fn main(){
-    println!("Factorial of 20 is {}", factorial(20));
-    println!("Factorial of 20 is {}", rec_factorial(20));
-}
-
-// Iterative factorial function
+/// Iterative factorial function
 fn factorial(mut number : u64) -> u64 {
     let mut fact : u64 = 1;
     
@@ -14,10 +9,16 @@ fn factorial(mut number : u64) -> u64 {
     fact
 }
 
-// Iterative factorial function
+/// Iterative factorial function
 fn rec_factorial(number : u64) -> u64 {
     if number > 0 {
         return number * rec_factorial(number - 1);
     }
     return 1;
+}
+
+#[test]
+fn test_factorials(){
+    asserteq!(2432902008176640000, factorial(20));
+     asserteq!(2432902008176640000, rec_factorial(20));
 }
